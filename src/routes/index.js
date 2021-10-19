f6d740af20458fcfcaddfd6ebfc0165d4517f877
DanielCO96 <cabreraojedadaniel@gmail.com>
2021-10-19 12:02:18 -0500

    add: Proyecto mercado liebre

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.sendFile(path.resolve(__dirname, '../views/index.html'))
+});
+
+router.get('/register', function(req, res, next) {
+    res.sendFile(path.resolve(__dirname, '../views/register.html'))
+});
+
+router.get('/login', function(req, res, next) {
+    res.sendFile(path.resolve(__dirname, '../views/login.html'))
+});
+module.exports = router;

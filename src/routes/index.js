e5603d3de16abcca5d8d6e2b90d157b10e2ffa7f
DanielCO96 <cabreraojedadaniel@gmail.com>
2021-10-21 16:58:48 -0500

    Archivo actualizado hasta la ejercitacion 7

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,11 +7,14 @@ router.get('/', function(req, res, next) {
     res.sendFile(path.resolve(__dirname, '../views/index.html'))
 });
 
+/* GET register page. */
 router.get('/register', function(req, res, next) {
     res.sendFile(path.resolve(__dirname, '../views/register.html'))
 });
 
+/* GET login page. */
 router.get('/login', function(req, res, next) {
     res.sendFile(path.resolve(__dirname, '../views/login.html'))
 });
+
 module.exports = router;
